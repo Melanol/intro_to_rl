643ff109110a7b24fe36b3e9f122db582eeb69bf
mel <callmesolis@gmail.com>
2022-07-04 19:28:05 +0500

    Finalize windy_gridworld.py

---
+++ environments/tests/test_windy_gridworld.py
@@ -0,0 +1,30 @@
+from environments.windy_gridworld import WindyGridworld, Human
+
+
+def test_not_jumping_over_goal():
+
+    class TestAgent:
+        def __init__(self, game):
+            self.game = game
+            self.x = None
+            self.y = None
+
+        def action_generator(self):
+            for action in ('right', 'right', 'right', 'right', 'right', 'right', 'right', 'right', 'right',
+                           'down', 'down', 'down', 'down', 'left', 'left'):
+                yield action
+
+    env = WindyGridworld()
+    agent = TestAgent(env)
+    env.agent = agent
+    action_iterator = agent.action_generator()
+
+    done = False
+    obs = env.reset()
+    step = 0
+    while not done:
+        action = next(action_iterator)
+        obs, reward, done = env.step(action)
+        step += 1
+
+    assert (obs, done, step) == ((7, 3), True, 15)
--- environments/windy_gridworld.py
+++ environments/windy_gridworld.py
@@ -1,16 +1,16 @@
-"""No discount. -1 for each step."""
+"""Page 130. The image in the book is confusing in wind power, so I had to improvise.
+No discount. -1 for each step."""
 
 
 import random
 
 
-class Coord:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class WindyGridworld:
 
-
-class Gridworld:
+    class Coord:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
 
     class WindyColumn:
         def __init__(self, x, strength):
@@ -42,12 +42,13 @@ class Gridworld:
         return obs, reward, done
 
     def step(self, action):
-        """1st we move, then we apply windy columns, then we make sure we are within the borders, then we
-        check if we have reached the goal.
-        """
+        reward = -1  # Reward is a constant
+        done = False
 
-        # Reward is a constant
-        reward = -1
+        # Windy columns
+        for windy_column in self.windy_columns:
+            if windy_column.x == self.agent.x:
+                self.agent.y -= windy_column.strength
 
         # Regular movement
         if action == 'up':
@@ -59,11 +60,6 @@ class Gridworld:
         elif action == 'right':
             self.agent.x += 1
 
-        # Windy columns
-        for windy_column in self.windy_columns:
-            if windy_column.x == self.agent.x:
-                self.agent.y -= windy_column.strength
-
         # Hitting walls
         if self.agent.x < 0:
             self.agent.x = 0
@@ -74,13 +70,34 @@ class Gridworld:
         elif self.agent.y > self.height - 1:
             self.agent.y = self.height - 1
 
-        # Reaching goal
-        done = False
+        # Check reaching goal
         if (self.agent.x, self.agent.y) == (self.goal.x, self.goal.y):
             done = True
 
         obs = self.agent.x, self.agent.y
         return obs, reward, done
+
+    def render(self):
+        """Render the grid in the console."""
+        for y in range(self.height):
+            for x in range(self.width):
+                if (x, y) == (self.agent.x, self.agent.y):
+                    print('X', end='  ')
+                elif (x, y) == (self.goal.x, self.goal.y):
+                    print('G', end='  ')
+                else:
+                    print('□', end='  ')
+            print()
+        for x in range(self.width):
+            found = False
+            for windy_column in self.windy_columns:
+                if windy_column.x == x:
+                    print(windy_column.strength, end='  ')
+                    found = True
+                    break
+            if not found:
+                print(0, end='  ')
+        print()
 
 
 class RandomAgent:
@@ -88,12 +105,6 @@ class RandomAgent:
         self.game = game
         self.x = None
         self.y = None
-        self.state_selection_n = {}
-        self.value_function = {}
-        for y in range(game.width):
-            for x in range(game.height):
-                self.state_selection_n[(x, y)] = 0
-                self.value_function[(x, y)] = 0
 
     def act(self, obs):
         return random.choice(self.game.action_space)
@@ -102,19 +113,42 @@ class RandomAgent:
         pass
 
 
+class Human:
+    def __init__(self, env):
+        self.env = env
+        self.x = None
+        self.y = None
+
+    def act(self, obs):
+        self.env.render()
+        while True:
+            action = input('Your move: ')
+            if action not in ('left', 'right', 'up', 'down'):
+                print('Action must be "left", "right", "up", or "down".')
+            else:
+                return action
+
+    def learn(self, action, obs):
+        pass
+
+
 def exe():
-    env = Gridworld()
-    agent = RandomAgent(env)
+    env = WindyGridworld()
+    # agent = RandomAgent(env)
+    agent = Human(env)
     env.agent = agent
+
     done = False
     obs = env.reset()
-    step = 1
+    step = 0
     while not done:
         action = agent.act(obs)
         obs, reward, done = env.step(action)
         agent.learn(action, reward)
         print(obs)
         step += 1
-    print(step)
+    print(f'Finished in {step} steps')
+
+
 if __name__ == '__main__':
     exe()
