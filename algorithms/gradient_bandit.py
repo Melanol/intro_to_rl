24ab5ebdbb586e6313a08972fc444623480beb4c
mel <callmesolis@gmail.com>
2022-07-05 07:29:28 +0500

    Make sarsa_for_windy_gridworld.py work

--- algorithms/gradient_bandit.py
+++ algorithms/gradient_bandit.py
@@ -15,11 +15,11 @@ class Agent:
         self.H = np.zeros(len(env.action_space))
         self.softmax()
 
-    def softmax(self):  # FIXME: Unugly
+    def softmax(self):
         self.pi = np.exp(self.H) / np.sum(np.exp(self.H))
 
-    def act(self):  # FIXME: Unugly
-        return np.random.choice(self.env.action_space, p=self.pi)
+    def act(self):
+        return np.random.choice(self.env.action_space, p=self.pi)  # Choosing randomly according to a prob distro
 
 
 def exe(alpha):
--- sarsa_for_windy_gridworld.py
+++ sarsa_for_windy_gridworld.py
@@ -8,31 +8,31 @@ from matplotlib import pyplot as plt
 from environments.windy_gridworld import WindyGridworld
 
 
-EPISODES = 200
+EPISODES = 100
 EPSILON = 0.1
 ALPHA = 0.5
 DISCOUNT = 1
-INITIAL_Q = 0
+DEFAULT_Q = 0
 
 
 class Sarsa:
     def __init__(self, env):
         self.env = env
-        # TODO: Generate Q as you encounter new states
+        self.Q = {}
 
-        # All actions for the terminal state have the value of 0
-        self._Q = {}
-        for action in env.action_space:
-            self._Q[((env.goal.x, env.goal.y), action)] = 0
+        # # All actions for the terminal state have the value of 0
+        # self._Q = {}
+        # for action in env.action_space:
+        #     self._Q[((env.goal.x, env.goal.y), action)] = 0
 
-    @property
-    def Q(self):
-        try:
-            return self._Q
-
-    @Q.setter
-    def Q(self, value):
-        self._Q = value
+    # @property
+    # def Q(self):
+    #     try:
+    #         return self._Q
+    #
+    # @Q.setter
+    # def Q(self, value):
+    #     self._Q = value
 
 
     def act(self, obs):
@@ -42,14 +42,15 @@ class Sarsa:
             max_action_value = -math.inf
             max_action = None
             for action in self.env.action_space:
-                action_value = self.Q[(obs, action)]
+                action_value = self.Q.get((obs, action), DEFAULT_Q)
                 if action_value > max_action_value:
                     max_action_value = action_value
                     max_action = action
             return max_action
 
-    def learn(self, obs, action, reward):
-        self.Q[(obs, action)] += ALPHA * (reward + DISCOUNT * self.Q[(obs, next_action)] - self.Q[(obs, action)])
+    def learn(self, obs, action, reward, next_obs, next_action):
+        self.Q[(obs, action)] = self.Q.get((obs, action), DEFAULT_Q) + ALPHA * \
+            (reward + DISCOUNT * self.Q.get((next_obs, next_action), DEFAULT_Q) - self.Q.get((obs, action), DEFAULT_Q))
 
     def print_Q(self):
         for key, value in list(self.Q.items())[2:-2]:
@@ -59,34 +60,19 @@ class Sarsa:
 env = WindyGridworld()
 agent = Sarsa(env)
 env.agent = agent
+steps_to_plot = []
 for _ in range(EPISODES):
     obs = env.reset()
     action = agent.act(obs)
     done = False
+    step = 0
     while not done:
-        obs, reward, done = env.step(action)
-        next_action = agent.act(obs)
-        agent.learn(obs, action, reward)
-        obs, action = obs, next_action
+        next_obs, reward, done = env.step(action)
+        next_action = agent.act(next_obs)
+        agent.learn(obs, action, reward, next_obs, next_action)
+        obs, action = next_obs, next_action
+        step += 1
+    steps_to_plot.append(step)
 
-
-# Plotting:
-
-# agent.print_Q()
-# ys_to_plot = []
-# for plot_actions in plot_actions_vs_episodes:
-#     ys_to_plot.append(sum(1 for el in plot_actions if el == 'right') / len(plot_actions))
-# plt.plot(ys_to_plot)
-#
-# count = 1
-# means_to_plot = []
-# ys = []
-# for y in ys_to_plot:
-#     ys.append(y)
-#     if count % 10 == 0:
-#         means_to_plot.append(sum(ys) / len(ys))
-#         ys = []
-#     count += 1
-# plt.plot([10 * x for x in range(EPISODES // 10)], means_to_plot)
-# plt.title('Mean of the rate of right actions chosen')
-# plt.show()
+plt.plot(steps_to_plot)
+plt.show()
